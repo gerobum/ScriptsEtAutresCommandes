ddbd52bdc6adb12140b81c767b9d31a0ac8ff947
yvan <yvan.maillot@gmail.com>
2016-10-12 22:39:46 +0200

    Ajout de quelques scripts

---
+++ compilerep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for j in *fascicule*.tex *article*.tex *presentation*.tex  *portrait*.tex *paysage*.tex *note*.tex
+do
+    if [ -f "$j" ] 
+    then
+        pdflatex -halt-on-error "$j"
+    fi
+done
+for j in *fascicule*.tex *article*.tex *presentation*.tex  *portrait*.tex *paysage*.tex *note*.tex
+do
+    if [ -f "$j" ] 
+    then
+        pdflatex -halt-on-error "$j"
+    fi
+done
+rm *.aux *.log *.out *.snm *.toc *.nav
