58e7cd9d39f4f62ee5bba0cf4a2968746e94ee77
yvan <yvan.maillot@gmail.com>
2016-11-04 00:39:06 +0100

    Modification de compilerep.sh pour supprimer les fichiers *.backup

--- compilerep.sh
+++ compilerep.sh
@@ -15,4 +15,4 @@ do
         pdflatex -halt-on-error "$j"
     fi
 done
-rm *.aux *.log *.out *.snm *.toc *.nav *.vrb > /dev/null 2>&1
+rm *.backup *.aux *.log *.out *.snm *.toc *.nav *.vrb > /dev/null 2>&1
