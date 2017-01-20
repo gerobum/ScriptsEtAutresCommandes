3022eadd22510a8fa1f923ceadc1f1ee62f29230
Yvan <yvan.maillot@uha.fr>
2017-01-20 18:13:55 +0100

    Avancement du projet d'analyse des logs

--- Java/LogFiles/src/log_gestion/Log.java
+++ Java/LogFiles/src/log_gestion/Log.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
  * @author yvan
  */
 public class Log {
-    
+
     private List<LogLine> liste = new LinkedList<>();
     private static final DateFormat DF = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
 
@@ -35,32 +35,32 @@ public class Log {
             }
         }
     }
-    
+
     public void clear() {
         liste.clear();
     }
-    
+
     public void quiEtaientConnectes(String salle, Date debut, Date fin) {
         List<LogLine> on = liste.stream()
                 .filter(p -> p.getDateTime().after(debut))
                 .filter(p -> p.getDateTime().before(fin))
                 .filter(p -> p.getSalle() != null ? p.getSalle().equals(salle) : p.getLieu().contains(salle))
-                .filter(p -> p.getStatus() == Statut.ON)                
+                .filter(p -> p.getStatus() == Statut.ON)
                 .collect(Collectors.toList());
         List<LogLine> off = liste.stream()
                 .filter(p -> p.getStatus() == Statut.OFF)
                 .collect(Collectors.toList());
-                
-        for(LogLine line : on) {
+
+        on.stream().forEach((line) -> {
             off.stream()
-                .filter(p -> p.getDateTime().after(debut))
-                .filter(p -> p.getDateTime().before(fin))
-                .filter(p -> p.getSalle() != null ? p.getSalle().equals(salle) : p.getLieu().contains(salle))
+                    .filter(p -> p.getDateTime().after(debut))
+                    .filter(p -> p.getDateTime().before(fin))
+                    .filter(p -> p.getSalle() != null ? p.getSalle().equals(salle) : p.getLieu().contains(salle))
                     .filter(p -> p.getNum().equals(line.getNum()))
                     .filter(p -> p.getDateTime().after(line.getDateTime()))
-                    .forEach(p -> { 
+                    .forEach(p -> {
                         System.out.println(line.getNum() + " en " + line.getLieu() + " de " + DF.format(line.getDateTime()) + " à " + DF.format(p.getDateTime()));
                     });
-        }
+        });
     }
 }
