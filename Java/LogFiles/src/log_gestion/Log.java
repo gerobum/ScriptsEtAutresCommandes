13e36ccdc921f5f70bc8c194c0955592691ded27
Yvan <yvan.maillot@uha.fr>
2017-01-20 18:13:55 +0100

    Avancement du projet d'analyse des logs

--- Java/LogFiles/src/log_gestion/Log.java
+++ Java/LogFiles/src/log_gestion/Log.java
@@ -7,8 +7,10 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -16,9 +18,76 @@ import java.util.stream.Collectors;
  */
 public class Log {
 
+    // logoff de e1401196 sur FST-I37-PC25 le 18/01/2017 a  8:07:48,08
     private List<LogLine> liste = new LinkedList<>();
-    private static final DateFormat DF = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+    private static final DateFormat DF = DateFormat.getTimeInstance(DateFormat.SHORT);
 
+    public class Connexion {
+
+        private String num;
+        private String lieu;
+        private String ufr;
+        private String salle;
+        private String poste;
+        private Date debut;
+        private Date fin;
+
+        public Connexion(String num, String lieu, String ufr, String salle, String poste, Date debut, Date fin) {
+            this.num = num;
+            this.lieu = lieu;
+            this.ufr = ufr;
+            this.salle = salle;
+            this.poste = poste;
+            this.debut = debut;
+            this.fin = fin;
+        }
+        
+        
+        
+        @Override
+        public String toString() {
+            if (ufr != null) {
+                return num + " sur le poste " + poste + " de " + DF.format(debut) + " à " + DF.format(fin);
+            } else {
+               return num + " en " + lieu + " de " + DF.format(debut) + " à " + DF.format(fin); 
+            }
+        }
+
+        public String getNum() {
+            return num;
+        }
+
+        public String getLieu() {
+            return lieu;
+        }
+
+        public String getUfr() {
+            return ufr;
+        }
+
+        public String getSalle() {
+            return salle;
+        }
+
+        public String getPoste() {
+            return poste;
+        }
+
+        public Date getDebut() {
+            return debut;
+        }
+
+        public Date getFin() {
+            return fin;
+        }
+        
+        
+    }
+
+    private List<Connexion> connexions = new LinkedList<>();
+    
+    private String salle = null;
+    
     public enum Statut {
         ON, OFF;
     }
@@ -41,6 +110,9 @@ public class Log {
     }
 
     public void quiEtaientConnectes(String salle, Date debut, Date fin) {
+        this.salle = salle;
+        connexions.clear();
+            
         List<LogLine> on = liste.stream()
                 .filter(p -> p.getDateTime().after(debut))
                 .filter(p -> p.getDateTime().before(fin))
@@ -52,15 +124,32 @@ public class Log {
                 .collect(Collectors.toList());
 
         on.stream().forEach((line) -> {
-            off.stream()
-                    .filter(p -> p.getDateTime().after(debut))
+            Optional<LogLine> opt = off.stream()
+                    .filter(p -> p.getDateTime().after(line.getDateTime()))
                     .filter(p -> p.getDateTime().before(fin))
                     .filter(p -> p.getSalle() != null ? p.getSalle().equals(salle) : p.getLieu().contains(salle))
                     .filter(p -> p.getNum().equals(line.getNum()))
                     .filter(p -> p.getDateTime().after(line.getDateTime()))
-                    .forEach(p -> {
-                        System.out.println(line.getNum() + " en " + line.getLieu() + " de " + DF.format(line.getDateTime()) + " à " + DF.format(p.getDateTime()));
-                    });
+                    .findFirst();
+            connexions.add(new Connexion(line.getNum(), line.getLieu(), line.getUfr(), line.getSalle(), line.getPoste(), line.getDateTime(), opt.get().getDateTime()));
         });
+    }
+    
+    @Override
+    public String toString() {
+        if (salle == null) {
+            return "{}";
+        } else {
+            StringBuilder sb = new StringBuilder("Salle ");
+            sb.append(salle).append('\n');
+            connexions.stream().forEach((c) -> {
+                sb.append(c).append('\n');
+            });
+            return sb.toString();
+        }
+    }
+    
+    public Stream<Connexion> stream() {
+        return connexions.stream();
     }
 }
--- Java/LogFiles/src/main/Main.java
+++ Java/LogFiles/src/main/Main.java
@@ -8,12 +8,16 @@ import sun.util.calendar.Gregorian;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Log logline = new Log();
-        logline.add(new File("logon_18_01_2017.log"));
-        logline.add(new File("logoff_18_01_2017.log"));
+        Log log = new Log();
+        log.add(new File("logon_18_01_2017.log"));
+        log.add(new File("logoff_18_01_2017.log"));
         GregorianCalendar debut = new GregorianCalendar(2017, 0, 18);
         GregorianCalendar fin = new GregorianCalendar(2017, 0, 19);
-        logline.quiEtaientConnectes("I37", debut.getTime(), fin.getTime());
+        log.quiEtaientConnectes("I37", debut.getTime(), fin.getTime());
+        GregorianCalendar apartir = new GregorianCalendar(2017, 0, 18, 9, 55, 0);
+        log.stream()
+                .filter(p -> !p.getFin().before(apartir.getTime()))
+                .forEach(System.out::println);
     }
  
 }
