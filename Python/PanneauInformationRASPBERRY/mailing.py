bfbc77491c4c28f16c583c876eb0077ef04cf438
yvan <yvan.maillot@gmail.com>
2017-02-17 23:02:52 +0100

    Panneau d'information
    Modication du traitement d'une exception

--- Python/PanneauInformationRASPBERRY/mailing.py
+++ Python/PanneauInformationRASPBERRY/mailing.py
@@ -66,172 +66,164 @@ class ReceiveMail(Thread):
                 # port=993
                 ssl=True,
             )    
-        except:
-            print "Unexpected error in gmail connect:", sys.exc_info()[0]
-            pass  
-               
-        try:
-            emails = box.folder('INBOX').emails(self.parent.q.unseen())
-                
-        except:
-            print "Unexpected error in box folder:", sys.exc_info()[0]
-            pass  
-        
-        
-        
+      
+            emails = box.folder('INBOX').emails(self.parent.q.unseen())             
 
-        if len(emails) > 0:    
-            for mail in emails:
-                mail.mark(['seen'])
-# ------------------------------------------------                
-                if mail['subject'] == 'ALL FONT':
-                    thesize = int(mail['text'][0]['text_normalized'])
-                    thefont = tkFont.Font(family='Helvetica',size=thesize, weight='bold')
-                    self.parent.parent.theBigfont = thefont
-                    self.parent.parent.theMiddlefont = thefont
-                    self.parent.parent.thefont = thefont
-                    self.parent.parent.lintro.config(font=thefont)             
-                    self.parent.parent.ldate.config(font=thefont)             
-                    self.parent.parent.lheure.config(font=thefont)
-                    for label in self.parent.parent.labels:
-                        label.config(font=thefont)
-# ------------------------------------------------  
-                elif mail['subject'] == 'COMMANDE':  
-                    cmd = mail['text'][0]['text_normalized']
-                    txt = commands.getoutput(cmd)
-                    send (self.parent.thename, self.parent.thepasswd, cmd, txt)
-# ------------------------------------------------  
-                elif re.match('DELAY ([0-9]+)', mail['subject']):
-                    try:
-                        m = re.search('DELAY ([0-9]+)', mail['subject'])
-                        p = int(m.group(1))
-                        if p >= 10:
-                            self.parent.mailing_delay = p
-                    except ValueError:
-                        pass
-# ------------------------------------------------  
-                elif mail['subject'] == 'FONT':
-                    thesize = mail['text'][0]['text_normalized']
-                    thefont = tkFont.Font(family='Helvetica',size=thesize, weight='bold')
-                    self.parent.ldate.config(font=thefont)
-# ------------------------------------------------  
-                elif mail['subject'] == 'GIT PULL REBOOT':
-                    commands.getoutput('git-pull-reboot')
-# ------------------------------------------------  
-                elif mail['subject'] == 'HELP':     
-                    try:
-                        with open('help.txt') as fp:
-                            message = []
-                            for line in fp:
-                                #line = line.decode('utf-8').encode('utf-8')
-                                message.append(line)
-                            send(self.parent.thename, self.parent.thepasswd, 'HELP', ''.join(message))
+            if len(emails) > 0:    
+                for mail in emails:
+                    mail.mark(['seen'])
+    # ------------------------------------------------                
+                    if mail['subject'] == 'ALL FONT':
+                        thesize = int(mail['text'][0]['text_normalized'])
+                        thefont = tkFont.Font(family='Helvetica',size=thesize, weight='bold')
+                        self.parent.parent.theBigfont = thefont
+                        self.parent.parent.theMiddlefont = thefont
+                        self.parent.parent.thefont = thefont
+                        self.parent.parent.lintro.config(font=thefont)             
+                        self.parent.parent.ldate.config(font=thefont)             
+                        self.parent.parent.lheure.config(font=thefont)
+                        for label in self.parent.parent.labels:
+                            label.config(font=thefont)
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'COMMANDE':  
+                        cmd = mail['text'][0]['text_normalized']
+                        txt = commands.getoutput(cmd)
+                        send (self.parent.thename, self.parent.thepasswd, cmd, txt)
+    # ------------------------------------------------  
+                    elif re.match('DELAY ([0-9]+)', mail['subject']):
+                        try:
+                            m = re.search('DELAY ([0-9]+)', mail['subject'])
+                            p = int(m.group(1))
+                            if p >= 10:
+                                self.parent.mailing_delay = p
+                        except ValueError:
+                            pass
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'FONT':
+                        thesize = mail['text'][0]['text_normalized']
+                        thefont = tkFont.Font(family='Helvetica',size=thesize, weight='bold')
+                        self.parent.ldate.config(font=thefont)
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'GIT PULL REBOOT':
+                        commands.getoutput('git-pull-reboot')
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'HELP':     
+                        try:
+                            with open('help.txt') as fp:
+                                message = []
+                                for line in fp:
+                                    #line = line.decode('utf-8').encode('utf-8')
+                                    message.append(line)
+                                send(self.parent.thename, self.parent.thepasswd, 'HELP', ''.join(message))
+                                    
+                        except TypeError as e:
+                            print "Type error({0})".format(e.message)   
+                            pass
+                        except IOError as e:
+                            print "I/O error({0}): {1}".format(e.errno, e.strerror) 
+                            pass                 
+                        except:
+                            print "Unexpected error:", sys.exc_info()[0]
+                            pass   
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'MINMAX':                        
+                        self.parent.parent.mini_maxi()
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'MSGS':  
+                        for line in mail['text'][0]['text_normalized'].replace('\r\n', '').replace('\n', '').replace('\\t', '\n').splitlines():                      
+                            self.parent.push(line.replace('\\n', '\n')) 
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'MSG':              
+                        self.parent.push(mail['text'][0]['text_normalized'].replace('\r\n', '').replace('\n', '').replace('\\n', '\n')) 
+    # ------------------------------------------------       
+                    elif re.match('REP ([0-9]+)', mail['subject']):
+                        try:
+                            m = re.search('REP ([0-9]+)', mail['subject'])
+                            p = int(m.group(1))                      
+                            self.parent.replace(p, mail['text'][0]['text_normalized'])
+                        except ValueError as v:  
+                            print 'mailing.py ReceiveMail run'
+                            print v
+                            sys.stderr.write(traceback.format_exc())   
+                            pass 
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'FICHIER':  
+                        try:                      
+                            for attachment in mail['attachments']:
+                                file_name = attachment['filename']
+                                data = attachment['data']
                                 
-                    except TypeError as e:
-                        print "Type error({0})".format(e.message)   
-                        pass
-                    except IOError as e:
-                        print "I/O error({0}): {1}".format(e.errno, e.strerror) 
-                        pass                 
-                    except:
-                        print "Unexpected error:", sys.exc_info()[0]
-                        pass   
-# ------------------------------------------------  
-                elif mail['subject'] == 'MINMAX':                        
-                    self.parent.parent.mini_maxi()
-# ------------------------------------------------  
-                elif mail['subject'] == 'MSGS':  
-                    for line in mail['text'][0]['text_normalized'].replace('\r\n', '').replace('\n', '').replace('\\t', '\n').splitlines():                      
-                        self.parent.push(line.replace('\\n', '\n')) 
-# ------------------------------------------------  
-                elif mail['subject'] == 'MSG':              
-                    self.parent.push(mail['text'][0]['text_normalized'].replace('\r\n', '').replace('\n', '').replace('\\n', '\n')) 
-# ------------------------------------------------       
-                elif re.match('REP ([0-9]+)', mail['subject']):
-                    try:
-                        m = re.search('REP ([0-9]+)', mail['subject'])
-                        p = int(m.group(1))                      
-                        self.parent.replace(p, mail['text'][0]['text_normalized'])
-                    except ValueError as v:  
-                        print 'mailing.py ReceiveMail run'
-                        print v
-                        sys.stderr.write(traceback.format_exc())   
-                        pass 
-# ------------------------------------------------  
-                elif mail['subject'] == 'FICHIER':  
-                    try:                      
-                        for attachment in mail['attachments']:
-                            file_name = attachment['filename']
-                            data = attachment['data']
-                            
-                            if len(data) < 500000: 
-                                with open(file_name, 'w') as f:
-                                    f.write(data)
-                    except:                        
-                        sys.stderr.write(traceback.format_exc()) 
-# ------------------------------------------------  
-                elif mail['subject'] == 'PHOTO':  
-                    try:                      
-                        for attachment in mail['attachments']:
-                            file_name = attachment['filename']
-                            data = attachment['data']
-                            content_type = attachment['content_type']
-                            
-                            if re.match('image[0-9]\.png', file_name) and len(data) < 500000 and content_type == 'image/png': 
-                                m = re.search('image([0-9])\.png', file_name)
-                                n = int(m.group(1))
-                                with open('images/'+file_name, 'w') as f:
-                                    f.write(data)
-                                self.parent.parent.maj_photo(n)
-                    except:                        
-                        sys.stderr.write(traceback.format_exc()) 
-# ------------------------------------------------  
-                elif mail['subject'] == 'SCREEN OFF':
-                    commands.getoutput('xset dpms force off')  
-# ------------------------------------------------    
-                elif mail['subject'] == 'SCREEN ON':
-                    commands.getoutput('xset dpms force on')  
-# ------------------------------------------------  
-                elif mail['subject'] == 'SCROT':  
-                    commands.getoutput('scrot screen.png')
-                    send(self.parent.thename, self.parent.thepasswd, 'Copie d\'écran', None, ['screen.png'])  
-# ------------------------------------------------  
-                elif mail['subject'] == 'STOP':
-                    self.parent.parent.the_end()
-# ------------------------------------------------  
-                elif re.match('SUP ([0-9]+)$', mail['subject']):
-                    try:
-                        m = re.search('SUP ([0-9]+$)', mail['subject'])
-                        p = int(m.group(1))                      
-                        self.parent.sup(p)
-                    except ValueError as v:  
-                        sys.stderr.write('mailing.py ReceiveMail run')
-                        sys.stderr.write(v)
-                        sys.stderr.write(traceback.format_exc())   
-                        pass
-                    except:  
-                        sys.stderr.write('mailing.py ReceiveMail run')
-                        sys.stderr.write(traceback.format_exc())   
-                        pass
-# ------------------------------------------------  
-                elif re.match('SUP ([0-9]+)-([0-9]+)', mail['subject']):
-                    try:
-                        m = re.search('SUP ([0-9]+)-([0-9]+)', mail['subject'])
-                        p = int(m.group(1))  
-                        q = int(m.group(2))                    
-                        self.parent.sup(p, q+1)
-                    except ValueError as v:  
-                        sys.stderr.write('mailing.py ReceiveMail run')
-                        sys.stderr.write(v)
-                        sys.stderr.write(traceback.format_exc())   
-                        pass
-                    except:  
-                        sys.stderr.write('mailing.py ReceiveMail run')
-                        sys.stderr.write(traceback.format_exc())   
-                        pass
-                   
-
-        box.logout()
+                                if len(data) < 500000: 
+                                    with open(file_name, 'w') as f:
+                                        f.write(data)
+                        except:                        
+                            sys.stderr.write(traceback.format_exc()) 
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'PHOTO':  
+                        try:                      
+                            for attachment in mail['attachments']:
+                                file_name = attachment['filename']
+                                data = attachment['data']
+                                content_type = attachment['content_type']
+                                
+                                if re.match('image[0-9]\.png', file_name) and len(data) < 500000 and content_type == 'image/png': 
+                                    m = re.search('image([0-9])\.png', file_name)
+                                    n = int(m.group(1))
+                                    with open('images/'+file_name, 'w') as f:
+                                        f.write(data)
+                                    self.parent.parent.maj_photo(n)
+                        except:                        
+                            sys.stderr.write(traceback.format_exc()) 
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'SCREEN OFF':
+                        commands.getoutput('xset dpms force off')  
+    # ------------------------------------------------    
+                    elif mail['subject'] == 'SCREEN ON':
+                        commands.getoutput('xset dpms force on')  
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'SCROT':  
+                        commands.getoutput('scrot screen.png')
+                        send(self.parent.thename, self.parent.thepasswd, 'Copie d\'écran', None, ['screen.png'])  
+    # ------------------------------------------------  
+                    elif mail['subject'] == 'STOP':
+                        self.parent.parent.the_end()
+    # ------------------------------------------------  
+                    elif re.match('SUP ([0-9]+)$', mail['subject']):
+                        try:
+                            m = re.search('SUP ([0-9]+$)', mail['subject'])
+                            p = int(m.group(1))                      
+                            self.parent.sup(p)
+                        except ValueError as v:  
+                            sys.stderr.write('mailing.py ReceiveMail run')
+                            sys.stderr.write(v)
+                            sys.stderr.write(traceback.format_exc())   
+                            pass
+                        except:  
+                            sys.stderr.write('mailing.py ReceiveMail run')
+                            sys.stderr.write(traceback.format_exc())   
+                            pass
+    # ------------------------------------------------  
+                    elif re.match('SUP ([0-9]+)-([0-9]+)', mail['subject']):
+                        try:
+                            m = re.search('SUP ([0-9]+)-([0-9]+)', mail['subject'])
+                            p = int(m.group(1))  
+                            q = int(m.group(2))                    
+                            self.parent.sup(p, q+1)
+                        except ValueError as v:  
+                            sys.stderr.write('mailing.py ReceiveMail run')
+                            sys.stderr.write(v)
+                            sys.stderr.write(traceback.format_exc())   
+                            pass
+                        except:  
+                            sys.stderr.write('mailing.py ReceiveMail run')
+                            sys.stderr.write(traceback.format_exc())   
+                            pass
+                          
+        except:
+            print "Unexpected error in ReceiveMail:", sys.exc_info()[0]
+            
+        finally:                  
+            box.logout()
 
 
 class Mailing(Thread):
